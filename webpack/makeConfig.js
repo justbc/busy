c1fe28873a695c109e508278eb6be535703d3c9c
justbc <justbc@gmail.com>
2018-03-20 14:14:10 -0700

    Update makeConfig.js

--- webpack/makeConfig.js
+++ webpack/makeConfig.js
@@ -154,6 +154,16 @@ function makeStyleLoaders(options) {
         ],
       }),
     },
+    {
+      test: /\.scss$/,
+      use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+            }, {
+              loader: "css-loader" // translates CSS into CommonJS
+            }, {
+              loader: "sass-loader" // compiles Sass to CSS
+            }]
+     }
   ];
 }
 
