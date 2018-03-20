9c05542f57db0c51e26053bb8e17b3c408be4a08
justbc <justbc@gmail.com>
2018-03-20 12:35:25 -0700

    Update configUtils.js

--- webpack/configUtils.js
+++ webpack/configUtils.js
@@ -1,6 +1,6 @@
 /**
  * Create a regular expressions for filename extensions.
  */
-exports.MATCH_CSS_LESS_SASS = /.(css|less|scss|sass)$/i;
+exports.MATCH_CSS_LESS = /.(css|less)$/i;
 exports.MATCH_JS_JSX = /.(js|jsx)$/i;
 
